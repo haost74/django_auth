094965a0747ef57be7b425b05b27dedfea3d6357
haost74 <haost@yandex.ru>
2022-04-20 14:26:47 +0300

    get user to views.py

--- django_project/views.py
+++ django_project/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 import datetime
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 def add_task_to_list(request, id):
+    user = get_user_model()
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    #html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>It is now %s.</body></html>" % request.user.username
     return HttpResponse(html)

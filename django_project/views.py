5cad890881df9b397355075c285e70a21830e060
haost74 <haost@yandex.ru>
2022-04-21 15:15:31 +0300

    check authorization

--- django_project/views.py
+++ django_project/views.py
@@ -3,24 +3,23 @@ import datetime
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.contrib.auth import logout
 from django.db import models
-
-Id = -1
 
 
 
 def add_task_to_list(request, name, id):
-    global Id
-    if (Id == -1):
-        Id = id
 
-    current_user = request.user
-    print(current_user, name, id)
+    current_user = request.user.username
+    print(current_user, name)
+    print(current_user == name)
 
     address = 'lessons' + str(id) + '.html'
-    address = 'lessons' + str(Id) + '.html'
-
-    if(Id == id):
+    #address = 'lessons' + str(Id) + '.html'
+    if(current_user == name):
         return render(request, 'lessons1.html')
     else:
+        logout(request)
         return render(request, 'home.html')
+
+

75676f08387b28c0a1051e95107acebfdd172a23
haost74 <haost@yandex.ru>
2022-04-21 12:14:02 +0300

    first lessons

--- django_project/views.py
+++ django_project/views.py
@@ -2,11 +2,8 @@ from django.http import HttpResponse
 import datetime
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.shortcuts import render
 
 
 def add_task_to_list(request, id):
-    user = get_user_model()
-    now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
-    html = "<html><body>It is now %s.</body></html>" % request.user.username
-    return HttpResponse(html)
+    return render(request, 'lessons1.html')

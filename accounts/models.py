187505434153a6610b9c2d14b1ad37d6bf6115d2
haost74 <haost@yandex.ru>
2022-04-22 10:46:18 +0300

    add UserModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    iduser = models.IntegerField (blank = True , null = True, default=0)
+    name = models.CharField(max_length=100)
+    lessonsmax = models.IntegerField(blank=True, null=True, default=1)
+
+    class Meta:
+        managed = True
+        db_table = 'accounts'
--- django_project/views.py
+++ django_project/views.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.contrib.auth import logout
+from accounts.models import UserModel
 from django.db import models
 
+#python manage.py makemigrations
+
+#python manage.py migrate --run-syncdb
 
 
 def add_task_to_list(request, name, id):
@@ -14,10 +18,19 @@ def add_task_to_list(request, name, id):
     print(current_user, name)
     print(current_user == name)
 
+    #UserModel.objects.filter(iduser=id).delete()
+
     address = 'lessons' + str(id) + '.html'
     #address = 'lessons' + str(Id) + '.html'
     if(current_user == name):
-        return render(request, 'lessons1.html')
+        cn = UserModel.objects.all().filter(iduser=id).count()
+        print(cn)
+        if(cn == 0):
+            u = UserModel(iduser = id, name = name)
+            u.save()
+        us = UserModel.objects.all().filter(iduser=id)
+        print(us[0].lessonsmax)
+        return render(request, 'lessons'+ str(us[0].lessonsmax) + '.html')
     else:
         logout(request)
         return render(request, 'home.html')

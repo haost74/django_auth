ad719601f96c0974532878c3608c8c7f2b8b24fc
Kristina-01 <gribkova01@bk.ru>
2022-04-27 20:43:42 +0300

    add lessons 3

--- django_project/data.py
+++ django_project/data.py
@@ -3,8 +3,10 @@ from accounts.models import UserModel
 def checkRes(request, us, num_page, isSave=True):
     if(num_page == 1):
        return lessons1(request, us, isSave)
-    else:
+    elif num_page == 2:
         return lessons2(request, us, isSave)
+    elif num_page == 3:
+        return lessons3(request, us, isSave)
     return False
 
 
@@ -14,6 +16,23 @@ def updateUserModel(isUpdate, us):
         us.lessonsmax = nextlessons
         UserModel.objects.all().filter(iduser=us.iduser).update(lessonsmax=nextlessons)
 
+def lessons3(request, us, isSave):
+    isRes = True
+    res1 = request.POST.get("test[1]")
+    res2 = request.POST.get("test[2]")
+    res3 = request.POST.get("test[3]")
+    res4 = request.POST.get("test[4]")
+    res5 = request.POST.get("test[5]")
+
+    if ('1' != res1): return False
+    if ('2' != res2): return False
+    if ('0' != res3): return False
+    if ('2' != res4): return False
+    if ('0' != res5): return False
+
+    if(isSave):
+        updateUserModel(isRes, us)
+    return isRes
 
 
 def lessons2(request, us, isSave):
